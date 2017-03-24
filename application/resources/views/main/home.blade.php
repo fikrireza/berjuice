63daa5522bff179008ecf992b9d612c0f1c9ea09
dessurya <fourline66@gmail.com>
2017-03-24 13:42:05 +0700

    add plugin and home

---
+++ application/resources/views/main/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Berri Juice</title>
+
+	<link rel="stylesheet" type="text/css" href="{{ URL::to('/') }}/plugin/bootstrap-3.3.7/bootstrap.css" />
+
+	<script src="{{ URL::to('/')}}/plugin/jquery/jquery-3.2.0.min.js"></script>
+
+</head>
+<body>
+
+	<script src="{{ URL::to('/')}}/plugin/angular-1.6/angular.min.js"></script>
+	<script src="{{ URL::to('/')}}/plugin/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
--- application/routes/web.php
+++ application/routes/web.php
@@ -12,10 +12,6 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/adsadas', function () {
-    return view('welcome');
+    return view('main.home');
 });
 
