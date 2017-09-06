1de370c6a7e82057645d18bdb83c408daf86deaf
dessurya <fourline66@gmail.com>
2017-09-06 11:33:53 +0700

    remove maps and branches in contact page

--- resources/views/frontend/contact-page/index.blade.php
+++ resources/views/frontend/contact-page/index.blade.php
@@ -16,6 +16,7 @@
 @endsection
 
 @section('content')
+	{{--
 	<div class="setup-wrapper">
 		<div class="wrapper-maps">
 			<iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4096047806356!2d106.62833971476904!3d-6.209582895504822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x320687ba53b4daa!2sPT.+Berri+Indosari!5e0!3m2!1sen!2sus!4v1503296702902" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -36,6 +37,7 @@
 			@endfor
 			<div class="clearfix"></div>
 	</div>
+	--}}
 
 	<div id="contact-us" class="setup-wrapper" style="background-image: url('{{ asset('amadeo/image/base/sakura.jpg') }}');">
 		<div class="setup-content lar-wd">
